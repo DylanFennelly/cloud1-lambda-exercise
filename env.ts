0134e3c2d7f711e4defe55605eecf8f7c4230299
Dylan Fennelly <74915458+DylanFennelly@users.noreply.github.com>
2023-11-30 18:46:53 +0000

    Added default emails to env.ts

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 export const SES_REGION = 'eu-west-1';
-export const SES_EMAIL_FROM = '20093427@mail.wit.ie' ;
-export const SES_EMAIL_TO =  '20093427@mail.wit.ie';
+export const SES_EMAIL_FROM = 'youremail@here.com' ;
+export const SES_EMAIL_TO =  'youremail@here.com';
